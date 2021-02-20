c852a0ae0447bd0f77f74ef3434c2a876e028005
Jeffrey777A <44594550+Jeffrey777A@users.noreply.github.com>
2021-02-20 04:53:29 +0000

    .

---
+++ dist/js/main.js
@@ -0,0 +1,12 @@
+// Options
+const key = "AIzaSyCoNktCpxuovL0GC2f8NIUWkiI20qDwGMo";
+const channel_id = "UCL9vywdtfTpzHabkEMxsu9A";
+
+const base_url = [
+  'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
+];
+const CHANNEL = 'https://www.googleapis.com/auth/youtube.readonly';
+
+
+
+const results = 10;
